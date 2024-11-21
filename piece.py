e0b86c89b9cf4b7e55f836e72f2abffb5a676ade
akinaybt <abekmambetova364@gmail.com>
2024-11-21 14:28:47 +0100

    Created class Piece with attributes

---
+++ piece.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Piece():
+    def __init__(self, position, colour, board):
+        self.position = position
+        self.position = position[0]
+        self.position = position[1]
+        self.colour = colour
+        self.board = board
+        self.made_move = False
+
