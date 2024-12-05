a1d81ed2b5325e6e604e5363384c6d89101fc242
akinaybt <abekmambetova364@gmail.com>
2024-12-05 18:41:32 +0100

    Added some pieces to be shown on the board

--- bishop.py
+++ bishop.py
@@ -2,8 +2,8 @@ import pygame
 from piece import Piece
 
 class Bishop(Piece):
-    def __init__(self, position, colour):
-        super().__init__("bishop", position, colour, 3.0)
+    def __init__(self, colour):
+        super().__init__("bishop", colour, 3.0)
 
-    def possible_moves(self, board):
+    # def possible_moves(self, board):
         
--- board.py
+++ board.py
@@ -1,9 +1,14 @@
 from const import *
+from pawn import Pawn
+from knight import Knight
+from bishop import Bishop
+from rook import Rook
+from queen import Queen
+from king import King
 from square import Square
 from piece import Piece
-from pawn import Pawn
 
-class Board():
+class Board:
     def __init__(self):
         self.squares = [[0, 0, 0, 0, 0, 0, 0, 0,] for col in range(COLS)]
 
@@ -18,6 +23,23 @@ class Board():
 
     def _add_pieces(self, colour):
         row_pawn, row_other = (6, 7) if colour == 'white' else (1, 0)
+
         # row of pawns
         for col in range(COLS):
             self.squares[row_pawn][col] = Square(row_pawn, col, Pawn(colour))
+
+        # knights
+        for col in range(COLS):
+            self.squares[row_other][1] = Square(row_other, 1, Knight(colour))
+            self.squares[row_other][6] = Square(row_other, 6, Knight(colour))
+
+        # bishops
+        for col in range(COLS):
+            self.squares[row_other][2] = Square(row_other, 2, Bishop(colour))
+            self.squares[row_other][5] = Square(row_other, 5, Bishop(colour))
+
+        # rooks
+        for col in range(COLS):
+            self.squares[row_other][0] = Square(row_other, 0, Rook(colour))
+            self.squares[row_other][7] = Square(row_other, 7, Rook(colour))
+
