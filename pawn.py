7dee7a23a375e13719a5ae420eafa31a5ec8079c
akinaybt <abekmambetova364@gmail.com>
2024-12-05 12:34:40 +0100

    Created class for all type of pieces

--- pawn.py
+++ pawn.py
@@ -2,9 +2,6 @@ import pygame
 from piece import Piece
 
 class Pawn(Piece):
-    def __init__(self, position, colour):
-        if colour == 'white':
-            self.direction = -1
-        else:
-            self.direction = 1
+    def __init__(self, colour, position=None):
+        self.direction = -1 if self.colour == 'white' else self.direction = 1
         super().__init__('pawn', position, colour, 1.0)
--- piece.py
+++ piece.py
@@ -1,11 +1,33 @@
 import pygame
+import os
 
 class Piece():
-    def __init__(self, position, colour, board):
+    def __init__(self, name, position, colour, value, image_url=None, board=None):
+        self.name = name
         self.position = position
-        self.position = position[0]
-        self.position = position[1]
+        self.x = position[0]
+        self.y = position[1]
         self.colour = colour
         self.board = board
+        self.value = value
+        self.image_url = image_url
+        self.set_image_url()
         self.made_move = False
+
+    def set_image_url(self):
+        self.image_url = os.path.join(
+            f'imgs/{self.colour}_{self.name}.png'
+        )
+
+    def move(self, new_position):
+        self.position = new_position
+
+    def possible_moves(self, board):
+        raise NotImplementedError("This method should be redefined in derived classes.")
+
+    def is_valid_move(self, position, board):
+        return position in self.possible_moves(board)
+
+
+
 
