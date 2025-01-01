4c0791afec8e74f597ec794cfe59a0110d19f6b2
grishkosofi <grishkosofi@gmail.com>
2025-01-01 22:12:35 +0300

    added the value in class piece and improved def move

--- piece.py
+++ piece.py
@@ -5,9 +5,11 @@ class Piece:
     def __init__(self, name, colour, value, image_url=None, image_rect=None):
         self.name = name
         self.colour = colour
-        self.value = value
+        value_sign = 1 if colour == 'white' else -1
+        self.value = value * value_sign
         self.image_url = image_url
         self.set_image_url()
+        self.moves = []
         self.made_move = False
         self.image_rect = image_rect
 
@@ -16,8 +18,8 @@ class Piece:
             f'imgs/{self.colour}_{self.name}.png'
         )
 
-    # def move(self, new_position):
-    #     self.position = new_position
+   # def move(self, new_position):
+   #    self.moves = new_position
 
     def possible_moves(self, board):
         raise NotImplementedError("This method should be redefined in derived classes.")
